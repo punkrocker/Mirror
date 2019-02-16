55a0a20e149016c25890abe0197bd5b340d65498
yuhaokui <punkrock_2000@163.com>
2019-02-16 23:03:57 +0800

    添加json解析

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 from urllib import request
+import json
 
 weather_info = request.urlopen("http://www.weather.com.cn/data/sk/101120201.html")
 readable_info = weather_info.read().decode('utf-8')
-print(readable_info)
-
+weather_json = json.loads(readable_info)
+print(weather_json['weatherinfo']['city'])
