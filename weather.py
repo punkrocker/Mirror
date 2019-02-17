a78c7be0ed6af086f24a98c98c6d3bd97acc4045
punkrocker <punkrock_2000@163.com>
2019-02-17 23:07:35 +0800

    封装温度类

--- weather.py
+++ weather.py
@@ -1,14 +1,16 @@
 from urllib import request
 import json
 
-city_code = '101120201'
-url = 'http://t.weather.sojson.com/api/weather/city/' + city_code
-weather_info = request.urlopen(url)
-readable_info = weather_info.read().decode('utf-8')
-weather_json = json.loads(readable_info)['data']
-print(weather_json)
-print('湿度：' + weather_json['shidu'])
-print('PM2.5：' + str(weather_json['pm25']))
-print('PM10：' + str(weather_json['pm10']))
-print('空气质量：' + weather_json['quality'])
-print('当前温度：' + str(weather_json['wendu']) + '℃')
+
+class Weather:
+    def __init__(self):
+        city_code = '101120201'
+        url = 'http://t.weather.sojson.com/api/weather/city/' + city_code
+        weather_info = request.urlopen(url)
+        readable_info = weather_info.read().decode('utf-8')
+        weather_json = json.loads(readable_info)['data']
+        self.wet = weather_json['shidu']
+        self.pm25 = str(weather_json['pm25'])
+        self.pm10 = str(weather_json['pm10'])
+        self.air_quality = weather_json['quality']
+        self.current_temperature = str(weather_json['wendu']) + '℃'
