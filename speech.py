6f5e9b01e30ac1fc03ad5a397d3b3ff4b46db8df
yuhaokui <punkrock_2000@163.com>
2019-05-21 21:14:23 +0800

    将语音合成方法封装成类

--- speech.py
+++ speech.py
@@ -1,17 +1,20 @@
 from aip import AipSpeech
 import appkey
 
-client = AipSpeech(appkey.APP_ID, appkey.API_KEY, appkey.SECRET_KEY)
 
+class Speech:
+    def __init__(self):
+        self.client = AipSpeech(appkey.APP_ID, appkey.API_KEY, appkey.SECRET_KEY)
 
-def makeMp3(content):
-    result = client.synthesis(content, 'zh', 1, {'spd': 0, 'vol': 5, 'per': 3})
-    # 识别正确返回语音二进制 错误则返回dict 参照下面错误码
-    filename = 'test'
-    if not isinstance(result, dict):
-        with open('文件的保存路径' + filename + '.mp3', 'wb') as f:
-            f.write(result)
+    def makeMp3(self, content):
+        result = self.client.synthesis(content, 'zh', 1, {'spd': 0, 'vol': 5, 'per': 3})
+        # 识别正确返回语音二进制 错误则返回dict 参照下面错误码
+        filename = 'test'
+        if not isinstance(result, dict):
+            with open('文件的保存路径' + filename + '.mp3', 'wb') as f:
+                f.write(result)
 
 
 if __name__ == '__main__':
-    makeMp3("你好")
+    speech = Speech()
+    speech.makeMp3("哈哈哈哈哈")
