026512b13dea58c3a44102500a89772384b5b408
punkrocker <punkrock_2000@163.com>
2019-06-22 21:18:59 +0800

    使用oled显示时间

--- oled.py
+++ oled.py
@@ -1,5 +1,25 @@
-from luma.core.interface.serial import i2c, spi
+from luma.core.interface.serial import i2c
 from luma.oled.device import sh1106
+from luma.core.render import canvas
+from PIL import ImageFont
+import time
 
-serial = i2c(port=1, address=0x3C)
-device = sh1106(serial)
+
+def stats(oled):
+    font = ImageFont.load_default()
+    with canvas(oled) as draw:
+        localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        draw.text((2, 5), localtime, font=font, fill=255)
+
+
+def main():
+    serial = i2c(port=1, address=0x3C)
+    oled = sh1106(serial)
+    while True:
+        stats(oled)
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
+
