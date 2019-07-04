a07b2498b40cea99009f619b134a12e669bb3425
punkrocker <yuhaokui@yuhaokui.com>
2019-07-05 00:35:01 +0800

    添加发送packet方法

--- computer/computer_controller.py
+++ computer/computer_controller.py
@@ -1,22 +1,19 @@
-import sys
+import wake_up
 
-def boot_computer():
+mac = '98:90:96:C1:FE:CB'
+
+
+def boot_computer(mac):
     try:
-        pyname, parameter = sys.argv
-        if parameter == '-h':
-            print('参数使用方法：python3 main_boot_computer.py mac地址\npython3 main_boot_computer.py 98:90:96:C1:FE:CB')
-        else:
-            print('正在向 ', parameter, ' 发送魔法唤醒包！')
-            # mac = '98:90:96:C1:FE:CB'
+        print('正在向 ', mac, ' 发送魔法唤醒包！')
+        mac = wake_up.format_mac(mac)
+        send_data = wake_up.create_magic_packet(mac)
 
-            mac = main_wake_on_lan.format_mac(parameter)
-            send_data = main_wake_on_lan.create_magic_packet(mac)
-
-            main_wake_on_lan.send_magic_packet(send_data)
-            return '成功向' + parameter + '发送唤醒包！'
+        wake_up.send_magic_packet(send_data)
+        return '成功向' + mac + '发送唤醒包！'
     except ValueError:
         print('未收到传入的参数\n获取帮助：python3 main_boot_computer.py -h')
 
 
 if __name__ == '__main__':
-    boot_computer()
+    boot_computer(mac)
--- computer/wake_up.py
+++ computer/wake_up.py
@@ -1,4 +1,22 @@
 import struct
+import socket
+import re
+
+
+def format_mac(mac):
+    mac_re = re.compile(r'''
+                      (^([0-9A-F]{1,2}[-]){5}([0-9A-F]{1,2})$
+                      |^([0-9A-F]{1,2}[:]){5}([0-9A-F]{1,2})$
+                      |^([0-9A-F]{1,2}[.]){5}([0-9A-F]{1,2})$
+                      )''', re.VERBOSE | re.IGNORECASE)
+    # print(re.match(mac_re, mac))
+    if re.match(mac_re, mac):
+        if mac.count(':') == 5 or mac.count('-') == 5 or mac.count('.'):
+            sep = mac[2]
+            mac_fm = mac.replace(sep, '')
+            return mac_fm
+    else:
+        raise ValueError('Incorrect MAC format')
 
 
 def create_magic_packet0(mac):
@@ -11,3 +29,12 @@ def create_magic_packet0(mac):
         send_data = send_data + struct.pack(b'B', int(data[i: i + 2], 16))  # int(data[i: i+2], 16) 把16进制转换成整数
     print(type(send_data))
     return send_data
+
+
+def send_magic_packet(send_data):
+    # broadcast_address = '192.168.255.255'
+    broadcast_address = '255.255.255.255'
+    port = 9
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.sendto(send_data, (broadcast_address, port))
