6d803cd0fd48fa4d74585015c6d679b5018d3832
punkrocker <punkrock_2000@163.com>
2019-12-27 22:48:14 +0800

    获取yeelight灯蓝牙服务

--- bluetooth/ble.py
+++ bluetooth/ble.py
@@ -1,4 +1,4 @@
-from bluepy.btle import Scanner, DefaultDelegate
+from bluepy.btle import Scanner, DefaultDelegate, Peripheral
 
 
 class ScanDelegate(DefaultDelegate):
@@ -12,10 +12,19 @@ class ScanDelegate(DefaultDelegate):
             print("Received new data from", dev.addr)
 
 
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(10.0)
+def scan_devices():
+    scanner = Scanner().withDelegate(ScanDelegate())
+    devices = scanner.scan(10.0)
+    for dev in devices:
+        print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+        for (adtype, desc, value) in dev.getScanData():
+            print("  %s = %s" % (desc, value))
 
-for dev in devices:
-    print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-    for (adtype, desc, value) in dev.getScanData():
-        print("  %s = %s" % (desc, value))
+
+if __name__ == '__main__':
+    p = Peripheral("b4:99:4c:60:94:42")
+    services = p.getServices()
+
+    # displays all services
+    for service in services:
+        print(service)
