fc88504aa08d3996895d77d32a132c43a6ef6f9b
punkrocker <punkrock_2000@163.com>
2020-01-05 20:09:49 +0800

    yeelight灯基本控制

--- light/yeelight_wifi.py
+++ light/yeelight_wifi.py
@@ -1,3 +1,10 @@
-from yeelight import discover_bulbs
+from yeelight import discover_bulbs, Bulb
+import time
+import random
 
-print(discover_bulbs())
+# print(discover_bulbs())
+bulb = Bulb("10.0.0.20")
+bulb.turn_on()
+while True:
+    bulb.set_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    time.sleep(5)
