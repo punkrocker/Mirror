71a5c9d17f2d6d422ab32284c4b48d66be439925
punkrocker <punkrock_2000@163.com>
2020-01-11 23:22:38 +0800

    提取开灯方法

--- light/yeelight_wifi.py
+++ light/yeelight_wifi.py
@@ -1,10 +1,9 @@
 from yeelight import discover_bulbs, Bulb
-import time
 import random
 
-# print(discover_bulbs())
-bulb = Bulb("10.0.0.20")
-bulb.turn_on()
-while True:
+
+def turn_on():
+    # print(discover_bulbs())
+    bulb = Bulb("10.0.0.20")
+    bulb.turn_on()
     bulb.set_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    time.sleep(5)
