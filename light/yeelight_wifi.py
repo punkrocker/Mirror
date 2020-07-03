59c6f10f205492109b80cf94e9cd1cb3ed3ad1e9
punkrocker <punkrock_2000@163.com>
2020-07-03 23:14:19 +0800

    临时提交，yeelight改名失败

--- light/yeelight_wifi.py
+++ light/yeelight_wifi.py
@@ -1,9 +1,13 @@
 from yeelight import discover_bulbs, Bulb
 import random
 
+lights = discover_bulbs()
 
-def turn_on():
-    # print(discover_bulbs())
-    bulb = Bulb("10.0.0.20")
+
+def turn_on(ip):
+    bulb = Bulb(ip)
     bulb.turn_on()
-    bulb.set_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    bulb.set_name("床头")
+    # bulb.set_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+print(lights)
+
