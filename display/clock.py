9b3b0cb64961e1c937fff6fca2b94ed30df8ff8b
punkrocker <punkrock_2000@163.com>
2020-07-08 23:57:18 +0800

    表改为时钟样式

--- display/clock.py
+++ display/clock.py
@@ -4,8 +4,15 @@
 import sh1106 as SH1106
 import time
 import datetime
-
+import math
 from PIL import Image, ImageDraw, ImageFont
+
+
+def position(angle, arm_length):
+    dx = int(math.cos(math.radians(angle)) * arm_length)
+    dy = int(math.sin(math.radians(angle)) * arm_length)
+    return dx, dy
+
 
 try:
     disp = SH1106.SH1106()
@@ -23,17 +30,34 @@ try:
     while True:
         image1 = Image.new('1', (disp.width, disp.height), "WHITE")
         draw = ImageDraw.Draw(image1)
-        draw.line([(0, 0), (127, 0)], fill=0)
-        draw.line([(0, 0), (0, 63)], fill=0)
-        draw.line([(0, 63), (127, 63)], fill=0)
-        draw.line([(127, 0), (127, 63)], fill=0)
         now = datetime.datetime.now()
         today_date = now.strftime("%d %b %y")
         today_time = now.strftime("%H:%M:%S")
         if today_time != today_last_time:
             today_last_time = today_time
-            draw.text((30, 0), today_date, font=font10, fill=0)
-            draw.text((28, 20), today_time, font=font, fill=0)
+            margin = 4
+            cx = 30
+            cy = 32
+
+            left = cx - cy
+            right = cx + cy
+
+            hrs_angle = 270 + (30 * (now.hour + (now.minute / 60.0)))
+            hrs = position(hrs_angle, cy - margin - 7)
+
+            min_angle = 270 + (6 * now.minute)
+            mins = position(min_angle, cy - margin - 2)
+
+            sec_angle = 270 + (6 * now.second)
+            secs = position(sec_angle, cy - margin - 2)
+
+            draw.ellipse((left + margin, margin, right - margin, 64 - margin), outline=0)
+            draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill=0)
+            draw.line((cx, cy, cx + mins[0], cy + mins[1]), fill=0)
+            draw.line((cx, cy, cx + secs[0], cy + secs[1]), fill=0)
+            draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2), fill=0, outline="white")
+            draw.text((2 * (cx + margin), cy - 8), today_date, fill=0)
+            draw.text((2 * (cx + margin), cy), today_time, fill=0)
             disp.ShowImage(disp.getbuffer(image1))
         time.sleep(0.1)
 
